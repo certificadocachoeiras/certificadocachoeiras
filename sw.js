85b4724f54b2c8d82246540eadd8cfceaab1e8ab
Lourran Martins <lourran.martinsfonseca@gmail.com>
2026-09-06 10:06:51 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'certificado-cachoeiras-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icone.png',
+  'https://unpkg.com/pdf-lib@1.17.1/dist/pdf-lib.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
